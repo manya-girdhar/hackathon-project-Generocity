c37693b881d88e9ade54eae2358608dbc0a82124
ayeshaa24 <56841086+ayeshaa24@users.noreply.github.com>
2020-07-26 00:23:02 +0100

    Delete firebase_config.py
    deleting again

--- generocity_flask_application/server/src/firebase_config.py
+++
@@ -1,9 +0,0 @@
-firebase_config = {
-  "apiKey": "AIzaSyB4lOxTmU0f9HWEgVjoFxdmsPjlnuWXxXs",
-  "authDomain": "generocity-bf0ef.firebaseapp.com",
-  "databaseURL": "https://generocity-bf0ef.firebaseio.com",
-  "projectId": "generocity-bf0ef",
-  "storageBucket": "generocity-bf0ef.appspot.com",
-  "messagingSenderId": "617915987724",
-  "appId": "1:617915987724:web:83c78fddc7f8166ac27b29"
-};
