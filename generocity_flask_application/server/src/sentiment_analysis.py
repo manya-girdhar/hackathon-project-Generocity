492e7c1977f5a06aa7ccb224005c17a66d4a4de4
Gmanya023 <gmanya023@gmail.com>
2020-07-25 18:45:12 +0300

    Code for sentiment analysis

---
+++ generocity_flask_application/server/src/sentiment_analysis.py
@@ -0,0 +1,26 @@
+# Call to the Natural language API for Sentiment Analysis
+
+# Import the google cloud client libraries
+from google.cloud import language
+from google.cloud.language import types # types module contains classes for creating requests
+from google.cloud.language import enums
+import os
+from keypath import keypath
+
+def analyse_text(project_description_input):
+    location = os.path.join(keypath,'keys.json')
+    client = language.LanguageServiceClient.from_service_account_json(location)
+    document = types.Document(content=project_description_input, type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+    score = annotations.document_sentiment.score
+    print("Overall Sentiment: score of ",score)
+    output = int((50*score)+50)
+    return output
+
+# project description from the form
+# Example text
+project_description_input = "Sundar Pichai said in his keynote that users love their new Android phones."
+
+# call the function and pass in the parameter
+output = analyse_text(project_description_input)
+print(output)
