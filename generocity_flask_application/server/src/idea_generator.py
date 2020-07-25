45a440af04678d3424140c154a88dff9f9ad8b15
Gmanya023 <gmanya023@gmail.com>
2020-07-25 17:56:54 +0300

    Kind acts generator backend code

---
+++ generocity_flask_application/server/src/idea_generator.py
@@ -0,0 +1,15 @@
+from flask import url_for
+import random
+# import string
+import pandas as pd
+
+def generate_idea():
+    # df = pd.read_csv("{{ url_for('static', filename='kind_acts.csv') }}")
+    df = pd.read_csv("../../static/kind_acts.csv")
+    list = df['Kind Acts'].tolist()
+    index = random.randint(0, len(list)-1)
+    idea = list[index]
+    return idea
+
+# call function
+print(generate_idea())
