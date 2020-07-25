9e4d744bb94bf1c4eda94f3bdbd49a801b7acce5
Ayesha <ayeshakhtar24@gmail.com>
2020-07-25 15:43:37 +0100

    created user login signup routes

--- generocity_flask_application/server/src/app.py
+++ generocity_flask_application/server/src/app.py
@@ -6,9 +6,20 @@ app = Flask(__name__, static_url_path="", static_folder="../../static/", templat
 
 # index route
 @app.route('/')
-@app.route("/home")
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/user")
+def user():
+    return render_template("user_homepage.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
